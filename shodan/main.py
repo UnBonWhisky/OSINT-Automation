2f28f8f77f11c1b69c446fc5da3bdcfdbb14d348
AlexiLRX <122472485+AlexiLRX@users.noreply.github.com>
2023-01-14 15:22:06 +0000

    shodan alexi v1

--- shodan/main.py
+++ shodan/main.py
@@ -1,26 +1,51 @@
 import shodan #chargement de la ibrairie shodan
+import requests
 
 #Pour que le script puisse utiliser l'API Shodan
 SHODAN_API_KEY = "jegaSstSh2fo9zNqKNHn7puZKoSoakQT"
 
 api = shodan.Shodan(SHODAN_API_KEY)
 
+print ("""Pour utiliser Shodan nous avons besoin d une adresse IP ou d un nom de domaine exacte
+                Veuillez taper :
+                '1' pour avoir des informations sur une adresse IP
+                '2' pour avoir des informations sur un nom de domaine
+                '3' pour quitter shodan  """)
+choix = int (input  ('Faites votre choix :'))
 
-a = input ('Entrez l adresse IP ou le nom de domaine de votre choix :')
+if choix > 3 :
+	choix = int (input  ('Faites votre choix seulement en tapant 1, 2 ou 3:'))
 
-# Lookup the host
+if choix == 1 :
+	a = input ('Veuillez entrer une adresse IP de votre choix : ')
+
+elif choix == 2 :
+	a = input ('Veuillez entrer un nom de domaine de votre choix : ')
+	#Si un nom de domain est entré, Shodan récolte l'adresse IP
+	dnsResolve = 'https://api.shodan.io/dns/resolve?hostnames=' + a + '&key=' + SHODAN_API_KEY
+	resolved = requests.get(dnsResolve) #Récuperer la sortie de dnsResolve
+	hostIP = resolved.json()[a] #Récupération de l'adresse IP
+	a = hostIP
+
+elif choix == 3 :
+	quit()
+
+#Analyse de l'adresse IP
 host = api.host(a)
 
-# Print general info
+# Affichage des différentes informations récoltés par l'API Shodan
 print("""
-        IP: {}
-        Organization: {}
-        Operating System: {}
+        Informations Générale :
+        Adresse IP : {}
+        Nom de l'entreprise : {}
+        Operating System : {}
+        Numéro de Système Autonome (ASN) : {}
+        Fournisseur Internet : {}
 
-        Location Information :
+        Information de géolocalisation :
         Pays : {}
         Region : {}
-        City : {}
+        Ville : {}
         Code Postale : {}
-        """.format(host['ip_str'], host.get('org', 'n/a'), host.get('os', 'n/a'),
-                  host.get('country_name', 'n/a'), host.get('region_code', 'n/a'), host.get('city', 'n/a'), host.get('postal_code', 'n/a') ))
+        """.format(host['ip_str'], host.get('org', 'n/a'), host.get('os', 'n/a'), host.get('asn', 'n/a'), host.get('isp', 'n/a'),
+                  host.get('country_name', 'n/a'), host.get('region_code', 'n/a'), host.get('city', 'n/a'), host.get('postal_code', 'n/a')))
